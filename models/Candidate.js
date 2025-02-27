3b1f034b972e2f1789971c04fb8592302a00f684
Piyush <piyushsalvi777@gmail.com>
2025-02-27 12:20:13 +0530

    form data

--- models/Candidate.js
+++ models/Candidate.js
@@ -7,13 +7,19 @@ const candidateSchema = new mongoose.Schema({
   address: { type: String, required: true, trim: true },
   qualification: { type: String, required: true, trim: true },
   experience: { type: String, required: true, trim: true },
-  email: { type: String, required: true, unique: true, trim: true, lowercase: true },
-  mobile: { 
-    type: String, 
-    required: true, 
-    unique: true, 
+  email: {
+    type: String,
+    required: true,
+    unique: true,
     trim: true,
-    match: [/^\d{10}$/, "Please enter a valid 10-digit mobile number"]
+    lowercase: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    match: [/^\d{10}$/, "Please enter a valid 10-digit mobile number"],
   },
   reference: { type: String, trim: true },
 });
